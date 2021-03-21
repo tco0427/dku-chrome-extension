da34d8c2a103c2a66f37e18af37d8e591e1d2916
JeongShin <sjeong1127@gmail.com>
2021-03-21 22:44:27 +0900

    env : project init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './assets/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">DKU Chrome Extension</h1>
+        </header>
+        <p className="App-intro">
+          단국대학교 크롬 익스텐션 입니다.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
