3a7e1bb17ec01b8379c4847eab1e1b621ed940b3
Jeong Shin <mac@Jeongui-MacBook-Pro-2.local>
2021-04-02 13:22:35 +0900

    Env : set eslintignore

--- src/content.js
+++ src/content.js
@@ -3,44 +3,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
-import App from "./App";
+import App from './App';
 
 class Main extends React.Component {
   render() {
     return (
-      <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
+      <Frame
+        head={[
+          <link
+            type="text/css"
+            rel="stylesheet"
+            href={chrome.runtime.getURL('/static/css/content.css')}
+          />,
+        ]}
+      >
         <FrameContextConsumer>
-          {
-            ({ document, window }) => {
-              return <App document={document} window={window} isExt={true} />
-            }
-          }
+          {({ document, window }) => {
+            return <App document={document} window={window} isExt={true} />;
+          }}
         </FrameContextConsumer>
       </Frame>
-    )
+    );
   }
 }
 
 const app = document.createElement('div');
-app.id = "my-extension-root";
+app.id = 'my-extension-root';
 
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
 
-app.style.display = "none";
+app.style.display = 'none';
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "clicked_browser_action") {
-      toggle();
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'clicked_browser_action') {
+    toggle();
   }
-);
+});
 
 function toggle() {
-  if (app.style.display === "none") {
-    app.style.display = "block";
+  if (app.style.display === 'none') {
+    app.style.display = 'block';
   } else {
-    app.style.display = "none";
+    app.style.display = 'none';
   }
 }
