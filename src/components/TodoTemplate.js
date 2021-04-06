6be69dcbf58ece65a10933cd7ad37841fe5dcd85
tco0427 <a29661498@gmail.com>
2021-04-06 17:02:41 +0900

    Feature: modify todo list template
    use scss -> styled-components

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,13 +1,36 @@
 /* eslint react/prop-types: 0 */
 import React from 'react';
-import './TodoTemplate.scss';
+// eslint-disable-next-line no-unused-vars
+import styled, { css } from 'styled-components';
 
+const Template = styled.div`
+  width: 512px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6rem;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const AppTitle = styled.div`
+  background: #22b8cf;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  background: white;
+`;
 const TodoTemplate = ({ children }) => {
   return (
-    <div className="TodoTemplate">
-      <div className="app-title">할 일 목록</div>
-      <div className="content">{children}</div>
-    </div>
+    <Template>
+      <AppTitle>할 일 목록</AppTitle>
+      <Content>{children}</Content>
+    </Template>
   );
 };
 
