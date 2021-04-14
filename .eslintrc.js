187ea7d64af2e93f6e9f6075dc811f2c7b65a5fa
YongsulLee <sadghnmb04@gmail.com>
2021-04-14 11:25:31 +0900

    Feature : add font

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
   rules: {
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
+    'linebreak-style': 0, 
   },
 };
