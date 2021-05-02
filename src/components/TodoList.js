a44b58cc10b542743fa9cd74036c25ceb303d12d
YongsulLee <sadghnmb04@gmail.com>
2021-05-02 14:26:45 +0900

    Ignore the changes

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoList = () => {
+    
+    return (
+        <div className="TodoInnerContainer">
+            <ul className="todo-list"></ul>           
+        </div>
+    )
+}
+
+export default TodoList
